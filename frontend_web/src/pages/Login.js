0635f8402e7514448375bd5cf672890241802e01
Kevin <119499728+kevintran73@users.noreply.github.com>
2024-10-04 11:35:18 +1000

    Changed img container in login page to only account for viewport width

--- frontend_web/src/pages/Login.js
+++ frontend_web/src/pages/Login.js
@@ -17,7 +17,7 @@ const StyledContainer = styled('div')({
 
 const ImgContainer = styled('div')({
   width: '40vw',
-  height: '90vh',
+  // height: '90vh',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
