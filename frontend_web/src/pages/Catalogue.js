0ae48501378bec1ffa798efad1e8453f99270df1
Kevin <119499728+kevintran73@users.noreply.github.com>
2024-10-07 16:56:07 +1100

    Moved nav bar and its child components into its own folder

--- frontend_web/src/pages/Catalogue.js
+++ frontend_web/src/pages/Catalogue.js
@@ -1,18 +1,11 @@
 import React from 'react';
-// import { useNavigate, Navigate, Link } from 'react-router-dom';
 
-// import TokenContext from '../context/TokenContext';
-import NavBar from '../components/NavBar';
+import NavBar from '../components/nav_bar/NavBar';
 
 /**
  * Catalogue/songs page
  */
 const Catalogue = () => {
-  //   const { token } = React.useContext(TokenContext);
-  //   if (token === null) {
-  //     return <Navigate to='/login' />;
-  //   }
-
   return (
     <>
       <NavBar></NavBar>
