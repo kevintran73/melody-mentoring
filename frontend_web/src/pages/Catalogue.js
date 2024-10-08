ac1ed503e1ca1432e1fbdf2cbf72a5fbad80a1b8
Vic <z5310452@ad.unsw.edu.au>
2024-10-09 05:32:49 +1100

    feat: added cards and titles to catalogue page

--- frontend_web/src/pages/Catalogue.js
+++ frontend_web/src/pages/Catalogue.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
+import Box from '@mui/material/Box';
 import NavBar from '../components/nav_bar/NavBar';
+import SongCard from '../components/catalogue/SongCard';
+import Typography from '@mui/material/Typography';
+
+import defaultImg from "../assets/default-img.png";
 
 /**
  * Catalogue/songs page
@@ -9,6 +14,69 @@ const Catalogue = () => {
   return (
     <>
       <NavBar></NavBar>
+      <Box margin="10px">
+        <Typography variant='h2' marginLeft="10px"> 
+          Playlist 1
+        </Typography>
+        <Box display="flex" flexDirection={'row'}>
+          <SongCard
+            title="Cold Cold Cold"
+            img={defaultImg}
+            artist="Cage the Elephant"
+            difficulty="7/10"
+          >
+          </SongCard>
+          <SongCard
+            title="Avant Gardener"
+            img={defaultImg}
+            artist="Courtney Barnett"
+            difficulty="5/10"
+          >
+          </SongCard>
+        </Box>
+      </Box>
+      <Box margin="10px">
+        <Typography variant='h2' marginLeft="10px"> 
+          Playlist 2
+        </Typography>
+        <Box display="flex" flexDirection={'row'}>
+          <SongCard
+            title="Cold Cold Cold"
+            img={defaultImg}
+            artist="Cage the Elephant"
+            difficulty="7/10"
+          >
+          </SongCard>
+          <SongCard
+            title="Avant Gardener"
+            img={defaultImg}
+            artist="Courtney Barnett"
+            difficulty="5/10"
+          >
+          </SongCard>
+        </Box>
+      </Box>
+      <Box margin="10px">
+        <Typography variant='h2' marginLeft="10px"> 
+          Playlist 3
+        </Typography>
+        <Box display="flex" flexDirection={'row'}>
+          <SongCard
+            title="Cold Cold Cold"
+            img={defaultImg}
+            artist="Cage the Elephant"
+            difficulty="7/10"
+          >
+          </SongCard>
+          <SongCard
+            title="Avant Gardener"
+            img={defaultImg}
+            artist="Courtney Barnett"
+            difficulty="5/10"
+          >
+          </SongCard>
+        </Box>
+      </Box>
     </>
   );
 };
